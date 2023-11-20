b638ed18af2f0df9c24c53a43052c21e4bd24afd
nikhea <imonikheaugbodaga@gmail.com>
2023-11-20 15:00:02 +0100

    added file to statictypying.ts

---
+++ statictyping.ts
@@ -0,0 +1,9 @@
+// JavaScript function without types
+function add(x, y) {
+  return x + y;
+}
+
+// TypeScript function with types
+function addWithType(x: number, y: number): number {
+  return x + y;
+}
